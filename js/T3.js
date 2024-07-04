c93cad6e5707bea900250002653f4ff4ca763007
reyprananda <rehanprananda7@gmail.com>
2024-07-04 20:35:42 +0700

    second commit

--- js/T3.js
+++ js/T3.js
@@ -1,4 +1,14 @@
-function tampilkanhasil() {
-    var nama = document.getElementById("nama").value;
-    alert("Nama kamu adalah: " + nama);
+function tampilanPesan() {
+    const namaLengkap = document.getElementById('namaLengkap').value;
+    const tanggalLahir = document.getElementById('date').value;
+    const jenisKelamin = document.querySelector('input[name="JenisKelamin"]:checked').value;
+    const pesan = document.getElementById('catatan').value;
+
+    const hasilDiv = document.getElementById('hasil');
+    hasilDiv.innerHTML = `
+        <p><strong>Nama Lengkap:</strong> ${namaLengkap}</p>
+        <p><strong>Tanggal Lahir:</strong> ${tanggalLahir}</p>
+        <p><strong>Jenis Kelamin:</strong> ${jenisKelamin}</p>
+        <p><strong>Pesan:</strong> ${pesan}</p>
+    `;
 }
